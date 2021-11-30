ef38e76efa8d48a54b44736cc37a8fb0f4c01226
ADUCOUDRE <antoine.ducoudre@ynov.com>
2021-11-30 02:24:10 +0100

    Can move without getting out of Array

--- src/fr/death/ynov/Game.java
+++ src/fr/death/ynov/Game.java
@@ -4,6 +4,15 @@ import java.util.Scanner;
 
 public class Game {
 
+    /*
+    TODO: CHANGER LE CODE D'ANTHO IL EST MOCHE --> OPTIMISATION
+    TODO: Manger les pions adverses
+    TODO: Condition de victoire/défaite
+    TODO: Menu Joueur Seul ou 2 Joueur --> Séléction 1 joueurs ou 2, explications des règles rapidement,
+     et sortir du jeu
+    TODO: Faire du 1 joueur --> code boucle sur pions "IA" et check si mouvement possible
+
+     */
     public void start() {
         final Map map = new Map();
         map.reset();
@@ -17,14 +26,11 @@ public class Game {
             Team testTurn=teamTurn(map,player1,player2,turn);
             Vector2D sideStep= scannerActualPawnPosition();
                   if(map.getPawn(sideStep).getOwner().getTeam()==testTurn){
-
-
-                      map.movePawn(map.getPawn(sideStep),
-                              scannerNewPawnPosition(testTurn,sideStep.getX(),sideStep.getY()));
-                        if(scannerNewPawnPosition(testTurn,sideStep.getX(),sideStep.getY())!=sideStep){
+                      Vector2D newPosition=scannerNewPawnPosition(testTurn,sideStep.getX(),sideStep.getY());
+                      map.movePawn(map.getPawn(sideStep),newPosition);
+                        if(newPosition!=sideStep){
                             turn+=1;
                         }
-
                   }
             displayMap(map);
         }
@@ -64,58 +70,83 @@ public class Game {
         Vector2D testPos= new Vector2D(x,y);
         Vector2D newPos= new Vector2D(0,0);
 
-        //Scanner sc = new Scanner(System.in);
-        int pos2=0;
-        boolean valid= false;
-        while(valid){
-            Scanner sc = new Scanner(System.in);
+
+
+            //Scanner sc = new Scanner(System.in);
             System.out.println("Veuillez choisir vos mouvements de pions :" +
                     "\n1 - Diagonale Gauche" +
                     "\n2 - Diagonale Droite");
-            if((x-1)>=0 || (x+1)<=9){
-                valid= true;
-                pos2 = sc.nextInt();
-            }
-            else{
-                valid=false;
-            }
-
-
+            System.out.println("OK");
+        Scanner sc = new Scanner(System.in);
+        int pos2 = sc.nextInt();
+        int testX= x+1;
+        int testNeg= x-1;
+        if(testX>9 && pos2!=1){
+        System.out.println("Vos seuls mouvements possibles:" +
+            "\n1 - Diagonale Gauche");
+        pos2 = sc.nextInt();
         }
+        if(testNeg<0 && pos2!=2){
+            System.out.println("Vos seuls mouvements possibles:" +
+                    "\n2 - Diagonale Droite");
+            pos2 = sc.nextInt();
+        }
+                //newPos.setX(x);
+                //newPos.setY(y);
+        /*switch (team) {
+            case WHITE:
+                switch (pos2) {
+                    case 1:
+                        newPos.setX(x - 1);
+                        newPos.setY(y + 1);
+                        break;
+                    case 2:
+                        newPos.setX(x + 1);
+                        newPos.setY(y + 1);
+                        break;
+                    default:
+                        newPos.setX(x);
+                        newPos.setY(y);
+                        System.out.println("Le pion ne peut pas sortir du tableau !");
 
+                }
+            case BLACK:
+                switch (pos2) {
+                    case 1:
+                        newPos.setX(x - 1);
+                        newPos.setY(y - 1);
+                        break;
+                    case 2:
+                        newPos.setX(x + 1);
+                        newPos.setY(y - 1);
+                        break;
+                    default:
+                        newPos.setX(x);
+                        newPos.setY(y);
+                        System.out.println("Le pion ne peut pas sortir du tableau !");
+                }
+        }*/
         if (team == Team.WHITE){
-            if( pos2==1 ){
+            if( pos2==1 && (x-1)>=0 ){
                 newPos.setX(x-1);
                 newPos.setY(y+1);
             }
-            else if (pos2==2 ){
+            else if (pos2==2 && (x+1)<=9){
                 newPos.setX(x+1);
                 newPos.setY(y+1);
             }
-            else {
-                newPos.setX(x);
-                newPos.setY(y);
-                System.out.println("Le pion ne peut pas sortir du tableau !");
-
-
-
-            }
+            else {newPos.setX(x);newPos.setY(y);System.out.println("Le pion ne peut pas sortir du tableau !");}
         }
         else if (team == Team.BLACK){
-            if (pos2==1 ){
+            if (pos2==1 && (x-1)>=0){
                 newPos.setX(x-1);
                 newPos.setY(y-1);
             }
-            else if (pos2==2 ){
+            else if (pos2==2 && (x+1)<=9){
                 newPos.setX(x+1);
                 newPos.setY(y-1);
             }
-            else {
-                newPos.setX(x);
-                newPos.setY(y);
-                System.out.println("Le pion ne peut pas sortir du tableau !");
-
-            }
+            else {newPos.setX(x);newPos.setY(y);System.out.println("Le pion ne peut pas sortir du tableau !");}
         }
 
         return newPos;
